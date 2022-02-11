ff7d8b6bc603a12b01f731d80646ce567cf2f9e8
CanderenKaymak <canderenkaymak@gmail.com>
2022-02-11 18:46:36 +0300

    number guessing game added

---
+++ java101/src/OtherTopics/NumberGuessGame.java
@@ -0,0 +1,48 @@
+package OtherTopics;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessGame {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        Scanner input = new Scanner(System.in);
+        int number = rnd.nextInt(100);
+        int right = 0;
+        int guessed;
+        int[] wrongNumbers = new int[5];
+        boolean isWin = false;
+        System.out.println(number);
+
+        while (right < 5) {
+            System.out.println("Tahmin ettiğiniz sayıyı giriniz : ");
+            guessed = input.nextInt();
+
+            if (guessed < 0 || guessed > 99) {
+                System.out.println("Lütfen 0-100 arasında bir değer giriniz");
+                continue;
+            }
+            if (guessed == number) {
+                isWin = true;
+                System.out.println("Tebrikler, Kazandınız..!");
+                break;
+            } else {
+
+                if (guessed > number) {
+                    System.out.println("Gizli Sayı Daha Küçük ;) ");
+                } else {
+                    System.out.println("Gizli Sayı Daha büyük ;) ");
+                }
+                wrongNumbers[right] = guessed;
+                right++;
+                System.out.println("Kalan Hakkın : " + (5 - right));
+            }
+        }
+        if (!isWin){
+            System.out.println("Kaybettin !!!");
+            System.out.println("Başarısız tahminleriniz : " + Arrays.toString(wrongNumbers));
+        }
+
+    }
+ }
+
